0b29e6172a48c7e3f1d365b789a9993c4d131d90
Bidrift <yassine.fighter@gmail.com>
2024-08-20 21:40:55 +0100

    Update main.rb Added a break in the loop with an option to replay or quit the game.

--- main.rb
+++ main.rb
@@ -1,5 +1,13 @@
 require_relative "lib/game"
 
+options = {
+  "0": "Restart the game",
+  "1": "Quit"
+}
+
 loop do
   Game.new
+  Prompter.put_options(options)
+  choice = Prompter.gets_match(/^[0-1]$/)
+  break if choice == "1"
 end
